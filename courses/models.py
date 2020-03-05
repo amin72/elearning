8adf29b5b93619e519ab3875cc2858baa72797e5
Amin <asaa47721@gmail.com>
2020-03-05 16:52:14 +0330

    fix mistake in Content model

--- courses/models.py
+++ courses/models.py
@@ -71,7 +71,7 @@ class Content(models.Model):
 
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
-    order = OrderField(blank=True, for_fields=[module])
+    order = OrderField(blank=True, for_fields=['module'])
 
     class Meta:
         ordering = ['order']
