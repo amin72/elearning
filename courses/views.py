ccb433443b7df5bf0aaf4660f13a31174073141f
Amin <asaa47721@gmail.com>
2020-03-05 23:59:27 +0330

    define ModuleOrderView and ContentOrderView views

--- courses/views.py
+++ courses/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth.mixins import (
 )
 from django.apps import apps
 from django.forms.models import modelform_factory
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
@@ -182,3 +183,23 @@ class ModuleContentListView(TemplateResponseMixin, View):
             course__owner=request.user)
 
         return self.render_to_response({'module': module})
+
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        print(self.request_json.items())
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        print(self.request_json.items())
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
