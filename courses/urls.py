27b84cadf1b8e4250954c585e61fa954b75aaafe
Amin <asaa47721@gmail.com>
2020-03-05 23:59:40 +0330

    add ModuleOrderView and ContentOrderView views to urls

--- courses/urls.py
+++ courses/urls.py
@@ -40,4 +40,12 @@ urlpatterns = [
     # list module contents
     path('module/<int:module_id>/', views.ModuleContentListView.as_view(),
         name='module_content_list'),
+
+    # order modules
+    path('module/order/', views.ModuleOrderView.as_view(),
+        name='module_order'),
+
+    # order contents
+    path('content/order/', views.ContentOrderView.as_view(),
+        name='content_order'),
 ]
