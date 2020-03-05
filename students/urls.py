7de51fee5e0f072f8db2c7047fc58b68c48f16ea
Amin <asaa47721@gmail.com>
2020-03-06 03:08:14 +0330

    add StudentEnrollCourseView view to urls

--- students/urls.py
+++ students/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = 'students'
 
 urlpatterns = [
+    # register student
     path('register/', views.StudentRegistrationView.as_view(),
         name='student_registration'),
+
+    # enroll course
+    path('enroll-course/', views.StudentEnrollCourseView.as_view(),
+        name='student_enroll_course'),
 ]
