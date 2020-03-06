9b10856a0b592611d7f5dd900374f48f2287cbfc
Amin <asaa47721@gmail.com>
2020-03-06 19:50:41 +0330

    define render method in ItemBase model

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 from .fields import OrderField
 
 
@@ -95,6 +97,11 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return self.title
+
+    def render(self):
+        return render_to_string(
+                f'courses/content/{self._meta.model_name}.html',
+            {'item': self})
 
 
 
