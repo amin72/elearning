848993edc8ee05982234a20c136160f86de1890f
Amin <asaa47721@gmail.com>
2020-03-07 23:35:04 +0330

    define CourseEnrollAPIView

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from ..models import Subject
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 
@@ -12,3 +15,11 @@ class SubjectListAPIView(generics.ListAPIView):
 class SubjectDetailAPIView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+
+class CourseEnrollAPIView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
