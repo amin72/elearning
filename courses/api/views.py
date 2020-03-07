798c9f909c585ac3207bccee8b80e2b0ce75047a
Amin <asaa47721@gmail.com>
2020-03-08 00:11:44 +0330

    define authentication_classes and permission_classes for CourseEnrollAPIView

--- courses/api/views.py
+++ courses/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
@@ -19,6 +21,9 @@ class SubjectDetailAPIView(generics.RetrieveAPIView):
 
 
 class CourseEnrollAPIView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
