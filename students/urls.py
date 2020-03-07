3eb1633012fd762cf698537b31e6bcd1d9ce64f8
Amin <asaa47721@gmail.com>
2020-03-07 19:11:29 +0330

    cache StudentCourseDetailView view using cache_page in students urls

--- students/urls.py
+++ students/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
 
@@ -18,10 +19,12 @@ urlpatterns = [
         name='student_course_list'),
 
     # student course detail
-    path('course/<pk>/', views.StudentCourseDetailView.as_view(),
+    path('course/<pk>/',
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail'),
 
     # student course detail with module
-    path('course/<pk>/<module_id>/', views.StudentCourseDetailView.as_view(),
+    path('course/<pk>/<module_id>/',
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail_module'),
 ]
