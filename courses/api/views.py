3e4f0d4e694066f6f053244c9ba35dfe4413b617
Amin <asaa47721@gmail.com>
2020-03-08 01:01:33 +0330

    Remove CourseEnrollAPIView and define enroll method in CourseViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
 
@@ -21,17 +22,14 @@ class SubjectDetailAPIView(generics.RetrieveAPIView):
 
 
 
-class CourseEnrollAPIView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
-
-
-
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @detail_route(method=['post'],
+                    authentication_classes=[BasicAuthentication],
+                    permission_classes=[IsAuthenticated])
+    def enroll(self, route, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
