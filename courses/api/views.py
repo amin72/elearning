21115647c0b2cfa31f0031f036659808e24e2330
Amin <asaa47721@gmail.com>
2020-03-08 02:53:43 +0330

    fix mistakes. define contents action in CourseViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -5,9 +5,14 @@ from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import action
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import (
+    SubjectSerializer,
+    CourseSerializer,
+    CourseWithContentSerializer,
+)
+from .permissions import IsEnrolled
 
 
 class SubjectListAPIView(generics.ListAPIView):
@@ -26,10 +31,20 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    @detail_route(method=['post'],
-                    authentication_classes=[BasicAuthentication],
-                    permission_classes=[IsAuthenticated])
-    def enroll(self, route, *args, **kwargs):
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        print('here\n')
+        return self.retrieve(request, *args, **kwargs)
