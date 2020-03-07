a14b0886f7f963d7076b56daeb28f518a2bc8302
Amin <asaa47721@gmail.com>
2020-03-08 00:24:05 +0330

    register a router for CourseViewSet

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 
 app_name = 'courses'
+
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
 
 urlpatterns = [
     # list subjects
@@ -12,6 +18,9 @@ urlpatterns = [
     path('subjects/<pk>/', views.SubjectDetailAPIView.as_view(),
         name='subject_detail'),
 
+    # enroll in a course
     path('courses/<pk>/enroll/', views.CourseEnrollAPIView.as_view(),
         name='course_enroll'),
+
+    path('', include(router.urls)),
 ]
